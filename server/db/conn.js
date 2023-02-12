ec089ef7eba5c67a81f9aafd2ec3fc78b1313caa
Meobot <nathanrhodes312@gmail.com>
2023-02-12 16:50:36 -0500

    database connected

--- server/db/conn.js
+++ server/db/conn.js
@@ -8,19 +8,21 @@ const client = new MongoClient(Db, {
 var _db;
  
 module.exports = {
-  connectToServer: function (callback) {
-    client.connect(function (err, db) {
-      // Verify we got a good "db" object
-      if (db)
-      {
-        _db = db.db("employees");
-        console.log("Successfully connected to MongoDB."); 
-      }
-      return callback(err);
-         });
-  },
- 
-  getDb: function () {
-    return _db;
-  },
-};
+	connectToServer: function (callback) {
+	  client.connect().then(function (db) {
+		if (db) {
+		  _db = db.db("sample-weatherdata");
+		  console.log("Successfully connected to MongoDB."); 
+		  console.log(_db)
+		}
+		return callback(null);
+	  }).catch(function (err) {
+		return callback(err);
+	  });
+	},
+  
+	getDb: function () {
+	  return _db;
+	},
+  };
+  
